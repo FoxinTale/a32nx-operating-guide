62d4e1a99cf2f9646149b8e646863eb191d426c5
Aubrey <51178662+FoxinTale@users.noreply.github.com>
2021-06-05 17:16:01 -0400

    Dimensions page implemented

--- scripts/ui.js
+++ scripts/ui.js
@@ -6,6 +6,20 @@ function doaccordion(id) {
     } else {
         x.className = x.className.replace("show", "");
         x.previousElementSibling.className =
-                x.previousElementSibling.className.replace("fbw-dark", "");
+            x.previousElementSibling.className.replace("fbw-dark", "");
     }
+}
+
+function opentab(evt, pagename) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("infotab");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" fbw-dark", "");
+    }
+    document.getElementById(pagename).style.display = "block";
+    evt.currentTarget.className += " fbw-dark";
 }
